3a4898d9502c24e341db5011a40a2836845c8da2
Yoan Dusoleil <y.dusoleil@outlook.com>
2024-03-18 08:55:03 +0100

    style: update vue d'articles et order de la liste d'article par la date

--- src/components/Articles/Article.tsx
+++ src/components/Articles/Article.tsx
@@ -13,25 +13,27 @@ function Article() {
     }, [state.filename])
 
     return (
-        <div>
-            <Markdown
-                components={{
-                    h1(props) {
-                        return <h1 className={"text-4xl font-bold"}>{props.children}</h1>
-                    },
-                    h2(props) {
-                        return <h2 className={"text-3xl font-bold"}>{props.children}</h2>
-                    },
-                    h3(props) {
-                        return <h3 className={"text-2xl font-bold"}>{props.children}</h3>
-                    },
-                    code(props) {
-                        return <code className={"bg-gray-200"}>{props.children}</code>
-                    }
-                }}
-            >
-                {content}
-            </Markdown>
+        <div className={"flex min-w-full justify-center items-center min-h-[90vh]"}>
+            <div className={"flex flex-col max-w-[60%] min-w-[60%] space-y-2.5"}>
+                <Markdown
+                    components={{
+                        h1(props) {
+                            return <h1 className={"text-4xl font-bold"}>{props.children}</h1>
+                        },
+                        h2(props) {
+                            return <h2 className={"text-3xl font-bold"}>{props.children}</h2>
+                        },
+                        h3(props) {
+                            return <h3 className={"text-2xl font-bold"}>{props.children}</h3>
+                        },
+                        code(props) {
+                            return <code className={"bg-gray-200"}>{props.children}</code>
+                        }
+                    }}
+                >
+                    {content}
+                </Markdown>
+            </div>
         </div>
     );
 }
--- src/components/Articles/ArticlesList.tsx
+++ src/components/Articles/ArticlesList.tsx
@@ -2,6 +2,13 @@ import {Card, CardDescription, CardHeader, CardTitle} from "@/components/ui/card
 import {useNavigate} from "react-router-dom";
 import {useEffect, useState} from "react";
 
+type Article = {
+    title: string,
+    description: string,
+    date: string,
+    filename: string
+}
+
 function ArticlesList() {
     const navigate = useNavigate();
     const [articles, setArticles] = useState([]);
@@ -10,14 +17,22 @@ function ArticlesList() {
         fetch("/Contents/contentList.json")
             .then((response) => (response.json()))
             .then((json) => setArticles(json))
-    }, [articles])
+    }, [])
+
+    console.log("Before sorting: ", articles)
+    articles.sort((a: Article, b: Article) => {
+        console.log(Date.parse(b.date) - Date.parse(a.date))
+        return Date.parse(b.date) - Date.parse(a.date)
+    })
+    console.log("After sorting: ", articles)
 
     return (
-        <div className={"flex flex-col w-full h-[90vh] justify-center items-center"}>
-            {articles.map((article: { title: String, description: String, date: String, filename: String }) => {
+        <div className={"flex flex-col w-full h-[90vh] justify-center items-center space-y-5"}>
+            {articles.map((article: Article) => {
                 return (
-                    <Card className={"hover:cursor-pointer min-w-96 max-w-96"}
+                    <Card className={"hover:cursor-pointer hover:shadow-primary min-w-96 max-w-96"}
                           onClick={() => (navigate('/article', {state: {filename: article.filename}}))}
+                          key={article.filename}
                     >
                         <CardHeader>
                             <CardTitle>{article.title}</CardTitle>
