58e1dad85311ea3c31eeecbaeff19d8318c3edc3
Yoan Dusoleil <y.dusoleil@outlook.com>
2024-03-19 16:06:13 +0100

    feat: update home page

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,6 +1,7 @@
 import {Button} from "@/components/ui/button.tsx";
 import {Github, Linkedin, Mail} from "lucide-react";
 import {Badge} from "@/components/ui/badge.tsx";
+import ProjetCard, {Projet} from "@/components/Home/ProjetCard.tsx";
 
 type Experience = {
     entreprise: string,
@@ -23,89 +24,156 @@ function Home() {
             entreprise: "Atol Conseils et Développements",
             poste: "Développeur Full Stack",
             periode: "2023 - En cours",
-            tags: ["Java", "React", "PostgreSQL", "JasperReports", "Docker", "PHP"],
-            description: "Développement d'applications web et mobiles pour des clients dans le secteur agricole. En parallèle, je réalise ma formation en alternance."
+            tags: ["Java", "React", "PostgreSQL"],
+            description: "Développement d'applications web et mobiles au sein de l'équipe Agri-Agro. Contrat d'alternance pendant ma formation en Master BDIA à l'université de Bourgogne."
         }
     ];
 
     const formations: Formation[] = [
         {
-            etablissement: "Université de Bourgogne",
-            diplome: "Master Bases de Données et Intelligence Artificielle",
+            etablissement: "Université de Bourgogne - Dijon",
+            diplome: "Master Informatique option Bases de Données et Intelligence Artificielle",
             periode: "2023 - En cours",
         },
         {
-            etablissement: "Université de Bourgogne",
+            etablissement: "Université de Bourgogne - Dijon",
             diplome: "Licence Informatique",
             periode: "2019 - 2023",
+        },
+        {
+            etablissement: "Lycée Christophe Colomb - Sucy-en-Brie",
+            diplome: "Baccalauréat STI2D option SIN",
+            periode: "2016 - 2019",
+        }
+    ];
+
+    const competences: string[] = [
+        "Java (Spring Boot/JAX-RS)", "C++ (Qt)", "React/React Native", "PHP", "PostgreSQL", "JasperReports", "Calculs distribués",
+        "Bases de données réparties", "Docker", "Git"
+    ];
+
+    const projets: Projet[] = [
+        {
+            nom: "Snoopy",
+            periode: "12/2023 - 03/2024",
+            tags: ["Java (Spring Boot)", "React", "PostgreSQL", "Apache POI", "Docker", "CI/CD"],
+            description: "Application web en React avec un backend en Spring-Boot pour envoyer des factures pdf à des clients. Déployé avec Docker.",
+            lien: "https://github.com/Yaon-C2H8N2/Snoopy"
+        },
+        {
+            nom: "N-Body Simulation",
+            periode: "10/2023 - 12/2023",
+            tags: ["distributed-systems", "C++", "MPI", "OpenMPI", "OpenGL", "NBody-Simulation"],
+            description: "Simulation à N-Corps développée en C++/MPI. Capable de gérer plus de 100k corps sur une douzaine de nœuds.",
+            lien: "https://github.com/Yaon-C2H8N2/Projet-Systemes-Distribues"
+        },
+        {
+            nom: "Cadmium",
+            periode: "10/2023 - 12/2023",
+            tags: ["TypeScript", "Angular", "TailwindCSS", "GitHub Actions", "CI/CD"],
+            description: "Todo list en Angular sans librairies de composants ni Backend. Tests Lint et CI/CD avec GitHub Actions.",
+            lien: "https://github.com/Yaon-C2H8N2/Projet-CWA"
         }
     ];
 
     return (
-        <div className={"flex w-full h-[90vh] overflow-y-auto justify-center items-center"}>
-            <div className={"flex flex-col max-w-[60%] min-w-[60%] space-y-5"}>
-                <div>
-                    <h1 className={"text-4xl font-bold"}>Yoan Dusoleil</h1>
+        <div className={"flex w-full justify-center"}>
+            <div className={"flex flex-col max-w-[60%] min-w-[50vh] space-y-5"}>
+                <div className={"flex flex-col gap-0.5"}>
+                    <h1 className={"text-4xl font-bold mb-2"}>Yoan Dusoleil</h1>
                     <div>Développeur Full Stack</div>
                     <div>Dijon, France</div>
-                    <div>
-                        <Button variant={"outline"} size={"icon"}>
-                            <Mail className={"w-6 h-6"}/>
-                        </Button>
-                        <Button variant={"outline"} size={"icon"}>
-                            <Github className={"w-6 h-6"}/>
-                        </Button>
-                        <Button variant={"outline"} size={"icon"}>
-                            <Linkedin className={"w-6 h-6"}/>
-                        </Button>
+                    <div className={"flex inline-flex gap-1"}>
+                        <a href={"mailto:y.dusoleil@outlook.com"}>
+                            <Button variant={"outline"} size={"icon"}>
+                                <Mail className={"w-6 h-6"}/>
+                            </Button>
+                        </a>
+                        <a href={"https://github.com/Yaon-C2H8N2"} target={"_blank"}>
+                            <Button variant={"outline"} size={"icon"}>
+                                <Github className={"w-6 h-6"}/>
+                            </Button>
+                        </a>
+                        <a href={"https://www.linkedin.com/in/yaon/"} target={"_blank"}>
+                            <Button variant={"outline"} size={"icon"}>
+                                <Linkedin className={"w-6 h-6"}/>
+                            </Button>
+                        </a>
                     </div>
                 </div>
                 <div>
-                    <h2 className={"text-3xl font-bold"}>À propos</h2>
-                    <div>
-                        Passionné par le développement informatique depuis mon plus jeune âge, j'ai maintenant la chance
-                        de
-                        pouvoir en vivre. Je suis actuellement en fin de formation en Master Bases de Données et
-                        Intelligence Artificielle à l'Université de Bourgogne.
+                    <h2 className={"text-3xl font-bold mb-2"}>À propos</h2>
+                    <div className={"text-justify"}>
+                        Etudiant de 23 ans, j'entame ma 1ère année de Master informatique option Bases de données et IA
+                        à l'université de Bourgogne. Passionné par l'informatique depuis plus d'une dizaine d'année et
+                        plus particulière par le développement Backend, j'exerce maintenant dans l'équipe Agri-Agro au
+                        sein d'Atol CD en tant que développeur Full-Stack sur les projets MesParcelles,
+                        MesCertifications et MonSimulateurPac. Vous trouverez ci-dessous mon CV et mes projets plus en
+                        détails. Vous pourrez également trouver des article plus détaillés dans l'onglet "Articles".
                     </div>
                 </div>
                 <div>
-                    <h2 className={"text-3xl font-bold"}>Expériences</h2>
+                    <h2 className={"text-3xl font-bold mb-2"}>Expériences</h2>
+                    <div className={"flex flex-col gap-1"}>
                     {experiences.map((experience: Experience) => {
                         return (
                             <div key={experience.entreprise}>
                                 <div className={"flex justify-between"}>
-                                    <div className={"flex flex-wrap space-x-1"}>
+                                    <div className={"flex flex-wrap gap-1 items-center"}>
                                         <h3 className={"text-xl font-bold"}>{experience.entreprise}</h3>
                                         {experience.tags.map((tag: string) => {
                                             return (
-                                                <Badge color={"secondary"}>{tag}</Badge>
+                                                <Badge key={"experience-" + tag} className={"h-5"}
+                                                       variant={"secondary"}>{tag}</Badge>
                                             )
                                         })}
                                     </div>
-                                    <div>{experience.periode}</div>
+                                    <div className={"min-w-[23vh] text-right"}>{experience.periode}</div>
                                 </div>
                                 <div>{experience.poste}</div>
-                                <div>{experience.description}</div>
+                                <div className={"text-justify"}>{experience.description}</div>
                             </div>
                         )
                     })}
+                    </div>
                 </div>
                 <div>
-                    <h2 className={"text-3xl font-bold"}>Formation</h2>
-                    {formations.map((formation: Formation) => {
-                        return (
-                            <div key={formation.diplome}>
-                                <div className={"flex justify-between"}>
-                                    <div className={"flex flex-wrap space-x-1"}>
+                    <h2 className={"text-3xl font-bold mb-2"}>Formations</h2>
+                    <div className={"flex flex-col gap-1"}>
+                        {formations.map((formation: Formation) => {
+                            return (
+                                <div key={formation.diplome}>
+                                    <div className={"flex justify-between"}>
                                         <h3 className={"text-xl font-bold"}>{formation.etablissement}</h3>
+                                        <div className={"min-w-[23vh] text-right"}>{formation.periode}</div>
                                     </div>
-                                    <div>{formation.periode}</div>
+                                    <div>{formation.diplome}</div>
                                 </div>
-                                <div>{formation.diplome}</div>
-                            </div>
-                        )
-                    })}
+                            )
+                        })}
+                    </div>
+                </div>
+                <div>
+                    <h2 className={"text-3xl font-bold mb-2"}>Compétences</h2>
+                    <div className={"flex flex-wrap gap-1"}>
+                        {competences.map((competence: string) => {
+                            return (
+                                <Badge key={"competence-" + competence} className={"h-5"}>{competence}</Badge>
+                            )
+                        })}
+                    </div>
+                </div>
+                <div>
+                    <h2 className={"text-3xl font-bold mb-2"}>Projets</h2>
+                    <div className={"flex flex-wrap gap-2 justify-center"}>
+                        {projets.map((projet: Projet) => {
+                            return (
+                                <div key={projet.nom} className={"max-w-[32%] min-w-[24vh] min-h-[30vh]"}>
+                                    <ProjetCard projet={projet}/>
+                                </div>
+                            )
+                        })}
+                    </div>
                 </div>
             </div>
         </div>
---
+++ src/components/Home/ProjetCard.tsx
@@ -0,0 +1,48 @@
+import {Card} from "@/components/ui/card.tsx";
+import {Badge} from "@/components/ui/badge.tsx";
+
+type Projet = {
+    nom: string,
+    tags: string[],
+    periode: string,
+    description: string
+    lien: string
+}
+
+interface ProjetCardProps {
+    projet: Projet
+}
+
+function ProjetCard(props: ProjetCardProps) {
+    return (
+        <>
+            <Card
+                className={"flex flex-col justify-between gap-2 w-full h-full p-3 hover:cursor-pointer hover:shadow-primary"}
+                onClick={() => {
+                    const anchor = document.createElement("a");
+                    anchor.href = props.projet.lien;
+                    anchor.target = "_blank";
+                    anchor.click();
+                }}
+            >
+                <div className={"flex flex-col gap-1"}>
+                    <h4 className={"text-xl font-bold"}>{props.projet.nom}</h4>
+                    <div className={"text-sm text-justify"}>{props.projet.description}</div>
+                    <div className={"flex flex-wrap gap-1"}>
+                        {props.projet.tags.map((tag: string) => {
+                            return (
+                                <Badge key={tag + "-tag-" + props.projet.nom} className={"h-5"}>{tag}</Badge>
+                            )
+                        })}
+                    </div>
+                </div>
+                <div className={"flex flex-inline justify-center"}>
+                    <div className={"text-xs min-w-[15vh]"}>{props.projet.periode}</div>
+                </div>
+            </Card>
+        </>
+    )
+}
+
+export type {Projet};
+export default ProjetCard;
